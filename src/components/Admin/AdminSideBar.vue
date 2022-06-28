af9ecba48857a760d9a4dc2044abf3693772bf43
Nguyễn Trọng Sang <33416398+ErogeMaster225@users.noreply.github.com>
2022-06-28 23:59:48 +0700

    :white_check_mark: Added admin site

---
+++ src/components/Admin/AdminSideBar.vue
@@ -0,0 +1,149 @@
+<script setup>
+	import { useUserStore } from "@/stores/userStore";
+	import { onMounted } from "vue";
+	import { useRouter, useRoute } from "vue-router";
+	const router = useRouter();
+	const store = useUserStore();
+	const logOut = () => {
+		store.$reset();
+		router.push("/");
+	};
+	const logIn = () => {
+		router.push("/auth/login");
+	};
+	const getUserInfoRequest = (username, token) => {
+		const response = fetch("https://vaporwaveapi.azurewebsites.net/api/User/userHomepage?userName=" + username, {
+			headers: {
+				Accept: "text/plain",
+				Authorization: "bearer " + token,
+			},
+		});
+		return response;
+	};
+	const getUserInfo = () => {
+		getUserInfoRequest(store.username, store.token)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error(response.statusText);
+			})
+			.then((data) => {
+				console.log(data);
+				store.balance = data.userWallet;
+				store.avatar = data.imageUri;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	onMounted(() => {
+		getUserInfo();
+	});
+</script>
+
+<template>
+	<div class="navBar">
+		<div class="logo">
+			<img src="/src/assets/logo.png" alt="" />
+			<div class="logoTextWrap">
+				<span>Vaporwave</span><br />
+				<span>Games Store</span>
+			</div>
+		</div>
+		<div class="userDetails" @click="$router.push('/profile')">
+			<img :src="store.avatar" alt="" />
+			<div class="details">
+				<div>{{ store.username }}</div>
+				<div>{{ "$" + store.balance.toFixed(2) }}</div>
+			</div>
+		</div>
+		<div class="navBtnWrapper">
+			<div class="navBarBtn" @click="$router.push('/admin')">
+				<i class="fa-regular fa-grid-2 fa-xl"></i>
+				<span>Dashboard</span>
+			</div>
+			<div class="navBarBtn" @click="$router.push('/admin/games')">
+				<i class="fa-regular fa-trophy fa-xl"></i>
+				<span>Games</span>
+			</div>
+			<div class="navBarBtn" @click="$router.push('/admin/users')">
+				<i class="fa-regular fa-user fa-xl"></i>
+				<span>User</span>
+			</div>
+			<div class="navBarBtn" id="logOut" @click="store.username == 'Guest' ? logIn() : logOut()">
+				<i class="fa-regular fa-power-off fa-xl"></i>
+				<span>{{ store.username == "Guest" ? "Log in" : "Log out" }}</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.navBar {
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		height: 100vh;
+		width: 20vw;
+		font-family: "Roboto";
+		color: #9198c4;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding-top: 40px;
+	}
+	img {
+		height: 50px;
+		width: 50px;
+		line-height: 50px;
+		float: left;
+		margin: 0 auto;
+	}
+	.logo span {
+		padding-left: 20px;
+		line-height: 25px;
+		text-overflow: clip;
+		margin: 0 auto;
+	}
+	.logoTextWrap {
+		display: inline-block;
+	}
+	.navBtnWrapper {
+		padding-left: 0;
+		padding-top: 20px;
+	}
+	.navBarBtn {
+		cursor: pointer;
+		padding: 5px 10px;
+		border-radius: 10px;
+	}
+	.navBarBtn span:hover {
+		color: #fff;
+	}
+	.navBarBtn i {
+		height: 50px;
+		width: 50px;
+		line-height: 50px;
+	}
+	#logOut {
+		position: absolute;
+		bottom: 0;
+	}
+	.userDetails {
+		color: #fff;
+		display: flex;
+		background-color: #9a84ff;
+		margin-top: 20px;
+		padding: 10px 30px 10px 10px;
+		border-radius: 7px;
+		cursor: pointer;
+	}
+	.userDetails img {
+		object-fit: cover;
+		border-radius: 7px;
+	}
+	.userDetails .details {
+		padding-left: 15px;
+	}
+</style>
---
+++ src/views/AdminView.vue
@@ -0,0 +1,62 @@
+<script setup>
+	import { onMounted, onUnmounted, onUpdated } from "vue";
+	import AdminSideBar from "@/components/Admin/AdminSideBar.vue";
+	onMounted(() => {
+		document.body.style.background = "#151921";
+	});
+	onUnmounted(() => {
+		document.querySelector("body").setAttribute("style", "");
+	});
+</script>
+
+<template>
+	<main>
+		<AdminSideBar />
+		<RouterView v-slot="{ Component, route }">
+			<Transition :name="route.meta.transition">
+				<component :is="Component" />
+			</Transition>
+		</RouterView>
+	</main>
+</template>
+<style>
+	body {
+		height: 100vh;
+		display: flex;
+		background-size: cover;
+		font-family: "Roboto", "Nunito Sans", sans-serif;
+		flex-direction: row;
+		padding: 0;
+	}
+	*::-webkit-scrollbar {
+		display: none;
+	}
+	.slide-fade-in-enter-active {
+		transition: opacity 0.3s ease-in, transform 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-in-leave-active {
+		transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-in-enter-from {
+		transform: translateX(80vw);
+		opacity: 0;
+	}
+	.slide-fade-in-leave-to {
+		transform: translateX(-100px);
+		opacity: 0;
+	}
+	.slide-fade-out-leave-active {
+		transition: opacity 0.3s ease-in, transform 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-out-enter-active {
+		transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.slide-fade-out-leave-to {
+		transform: translateX(80vw);
+		opacity: 0;
+	}
+	.slide-fade-out-enter-from {
+		transform: translateX(-100px);
+		opacity: 0;
+	}
+</style>
