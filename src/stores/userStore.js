428557fe373c527b382755f768a3d0d45e161983
Nguyễn Trọng Sang <33416398+ErogeMaster225@users.noreply.github.com>
2022-06-29 00:02:22 +0700

    :white_check_mark: It's opening day :tada:

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -3,9 +3,12 @@ export const useUserStore = defineStore("user", {
 	state: () => {
 		return {
 			username: "Guest",
-			balance: 0.00,
+			balance: 0.0,
 			avatar: "https://osu.ppy.sh/images/layout/avatar-guest@2x.png",
 			token: "",
+			dob: "",
+			email: "",
+			phone: "",
 		};
 	},
 	actions: () => {},
